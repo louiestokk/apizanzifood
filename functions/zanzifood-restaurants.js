441705011186848a118b2ad07e111115c9289714
Your Name <louiestokk>
2022-02-22 18:30:08 +0100

    cors fix

--- functions/zanzifood-restaurants.js
+++ functions/zanzifood-restaurants.js
@@ -4,6 +4,8 @@ exports.handler = async () => {
   return {
     headers: {
       "Access-Control-Origin": "*",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept",
     },
     statusCode: 200,
     body: JSON.stringify(restaurants),
